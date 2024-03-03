618e8f4729b4ec6dbf4e979c9060e4ca79d1d057
vv90 <vladimir.kirienko.e@gmail.com>
2024-03-03 00:24:00 +0000

    archive logs (#53)
    * archive logs
    
    * precommit fix

--- server/src/Aprs/Utils.hs
+++ server/src/Aprs/Utils.hs
@@ -3,6 +3,8 @@ module Aprs.Utils where
 import Aprs.AprsMessage (AprsMessage (..), DeviceId, aprsMessageParser, getDeviceId)
 import Aprs.LocationDatapoint (fromAprsMessage)
 import Backend.FlightsState (FlightInformation, FlightPosition (..), makeFlightInformation, toFlightPosition)
+import Codec.Compression.GZip (CompressParams (compressLevel))
+import Codec.Compression.GZip qualified as GZip
 import Conduit (ConduitT, await, runConduit, yield, (.|))
 import Control.Concurrent (threadDelay)
 import Control.Concurrent.Async (concurrently_)
@@ -10,13 +12,13 @@ import Control.Concurrent.STM.TBChan (TBChan, isFullTBChan, readTBChan, writeTBC
 import Data.Aeson qualified as Aeson
 import Data.Conduit.Combinators (linesUnboundedAscii)
 import Data.Conduit.Network (AppData, appSink, appSource, clientSettings, runTCPClient)
-import Data.HashMap.Strict as HM
+import Data.HashMap.Strict qualified as HM
 import Data.Time (UTCTime (utctDay), getCurrentTime, utctDayTime)
 import Data.UUID (UUID)
 import Glidernet.DeviceDatabase (DeviceInfo)
 import Relude
-import System.Directory (createDirectoryIfMissing)
-import System.FilePath ((<.>), (</>))
+import System.Directory (createDirectoryIfMissing, doesDirectoryExist, listDirectory, removeDirectory, removeFile)
+import System.FilePath (takeExtension, (<.>), (</>))
 import Text.Parsec (parse)
 import TimeUtils (diffTimeToSeconds)
 
@@ -46,8 +48,40 @@ isPositionRecentEnough currTimeSeconds (FlightPosition {timeSeconds}) =
       timeDiffSeconds = currTimeSeconds - timeSeconds -- negative time difference means the position is from the previous day
    in timeDiffSeconds < 1800 && timeDiffSeconds >= 0 -- less than 30 minutes old and on the same day
 
+compressFiles :: FilePath -> IO ()
+compressFiles dirName =
+  let compressionParams :: CompressParams
+      compressionParams = GZip.defaultCompressParams {compressLevel = GZip.bestCompression}
+
+      dirPath :: FilePath
+      dirPath = logsDirectory </> dirName
+
+      archivePath :: FilePath
+      archivePath = logsDirectory </> "archive" </> dirName
+
+      compressAndRemove :: FilePath -> IO ()
+      compressAndRemove fileName =
+        let filePath :: FilePath
+            filePath = dirPath </> fileName
+         in do
+              contents <- readFileLBS filePath
+              createDirectoryIfMissing True archivePath
+              writeFileLBS (archivePath </> fileName <.> "gz") $ GZip.compressWith compressionParams contents
+              removeFile filePath
+   in do
+        files <- filter ((== ".json") . takeExtension) <$> listDirectory dirPath
+        traverse_ compressAndRemove files
+
+        leftover <- listDirectory dirPath
+        when (null leftover) $ removeDirectory dirPath
+
 cleanUpFlightsState :: TVar FlightsState -> IO ()
 cleanUpFlightsState flights = do
+  todaysDirectoryName <- show . utctDay <$> getCurrentTime
+  dirs <- listDirectory logsDirectory >>= filterM (\dir -> doesDirectoryExist (logsDirectory </> dir))
+
+  traverse_ compressFiles (filter (/= todaysDirectoryName) dirs)
+
   currTimeSeconds <- round . diffTimeToSeconds . utctDayTime <$> getCurrentTime
   atomically $ modifyTVar' flights (HM.filter (isPositionRecentEnough currTimeSeconds . snd))
 
